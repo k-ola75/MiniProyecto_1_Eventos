76040c1a942d96b25c051db4c518a05db1cbf0a8
k-ola75 <98629226+k-ola75@users.noreply.github.com>
2022-10-19 15:46:00 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 /*
-Juan Sebastian Oviedo Oviedo 2179238-2724 / Juan.sebastian.oviedo@orreounivalle.edu.co
+  Autores:
+    Juan Sebastian Oviedo Oviedo (2179238-2724)
+    Kirk Olaya Villamarín (2178638-2724)
+  E-mail:
+    Juan.sebastian.oviedo@orreounivalle.edu.co
+    kikr.olaya@correounivalle.edu.co
+  Fecha: 2022-10-19
+  Propósito: Académico
+  Versión actual: 1.0
+  Versión anterior: -
 */
 
 import game.MenuPpal;

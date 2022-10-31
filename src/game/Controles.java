03b23d65c0b92f45ffe4efab92e72bf6ab236389
Sebas_PC_home <juan.sebastian.oviedo@correounivalle.edu.co>
2022-10-31 12:16:34 -0500

    pequeña refacturacion

--- src/game/Controles.java
+++ src/game/Controles.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -8,7 +9,6 @@ public class Controles implements KeyListener {
     public Mapas mapa;
 
     public Controles(Figuras jugador, Mapas mapa) {
-
         this.jugador = jugador;
         this.mapa = mapa;
     }
@@ -21,30 +21,40 @@ public class Controles implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == e.VK_UP) {
-            if (jugador.y > Constantes.ALTURA_BARRA_TAREAS) {
-                if (!mapa.getMapa((int) (jugador.x / 25), (int) ((jugador.y - 1) / 25)).equals("w"))
-                    jugador.y -= 25;
-            }
-        } else if (e.getKeyCode() == e.VK_DOWN) {
-            if (jugador.y + jugador.largo < Constantes.LARGO_LABERINTO) {
-                if (!mapa.getMapa((int) (jugador.x / 25), (int) ((jugador.y + 25) / 25)).equals("w"))
-                    jugador.y += 25;
-            }
-        } else if (e.getKeyCode() == e.VK_LEFT) {
-            if (jugador.x > Constantes.BORDE_IZQUIERDO) {
-                if (!mapa.getMapa((int) ((jugador.x - 1) / 25), (int) (jugador.y / 25)).equals("w"))
-                    jugador.x -= 25;
-            }
-        } else if (e.getKeyCode() == e.VK_RIGHT) {
-            if (jugador.x + jugador.ancho < Constantes.ANCHO_LABERINTO)
-                if (!mapa.getMapa((int) ((jugador.x + 25) / 25), (int) (jugador.y / 25)).equals("w"))
-                    jugador.x += 25;
+
+        switch ( e.getKeyCode() ){
+            case 38://arriba
+                if (jugador.y > Constantes.ALTURA_BARRA_TAREAS) {
+                    if (!mapa.getMapa((int) (jugador.x / 25), (int) ((jugador.y - 1) / 25)).equals("w"))
+                        jugador.y -= 25;
+                }
+                break;
+            case 40://abajo
+                if (jugador.y + jugador.largo < Constantes.LARGO_LABERINTO) {
+                    if (!mapa.getMapa((int) (jugador.x / 25), (int) ((jugador.y + 25) / 25)).equals("w"))
+                        jugador.y += 25;
+                }
+                break;
+            case 37://izquierda
+                if (jugador.x > Constantes.BORDE_IZQUIERDO) {
+                    if (!mapa.getMapa((int) ((jugador.x - 1) / 25), (int) (jugador.y / 25)).equals("w"))
+                        jugador.x -= 25;
+                }
+                break;
+            case 39://derecha
+                if (jugador.x + jugador.ancho < Constantes.ANCHO_LABERINTO){
+                    if (!mapa.getMapa((int) ((jugador.x + 25) / 25), (int) (jugador.y / 25)).equals("w"))
+                        jugador.x += 25;
+                }
+                break;
         }
+
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
 
     }
+
+
 }
